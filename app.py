0758d174273b191670733245b033fbb527354b75
Hsiao Yao <tomtogo1260@gmail.com>
2023-12-28 19:48:39 +0800

    use another cropping approach & code refactor

--- app.py
+++ app.py
@@ -70,94 +70,40 @@ def gen_pos_map(w, h, x_max, y_max, z_max):
     position_map = position_map[:, :, [2, 1, 0, 3]]
     cv2.imwrite(pos_map_name, position_map, [cv2.IMWRITE_PNG_COMPRESSION, 9])
 
-# Generate multiple sub mask (clustering)
-def clustering(x_max, y_max, z_max):
+# Generate uv map for each sub segment
+def gen_sub_uv(x_max, y_max, z_max):
     center_name  = 'scroll1_center.obj'
     pos_map_name = '20230702185753.png'
     segmentID    = '20230702185753'
 
     # load scroll center into (used for cutting)
     data    = parse_obj(center_name)
-    center  = data['vertices']
-    center /= np.array([x_max, y_max, z_max])
+    scroll_center  = data['vertices']
+    scroll_center /= np.array([x_max, y_max, z_max])
 
     # load position map (rgba 0~1)
     position_map = cv2.imread(pos_map_name, cv2.IMREAD_UNCHANGED)
-    position_map = position_map[:, :, [2, 1, 0, 3]]
+    position_map = cv2.cvtColor(position_map, cv2.COLOR_BGRA2RGBA)
     position_map = position_map.astype(np.float32) / 65535
 
-    # cutting along the scroll center (opacity change along the cutting edge)
-    edge_x = np.interp(position_map[:, :, 2], center[:, 2], center[:, 0])
+    # uv map
+    h, w = position_map.shape[:2]
+    alpha = position_map[:, :, 3]
+    u, v = np.meshgrid(np.linspace(0, 1, w), np.linspace(0, 1, h))
+    uv_map = np.dstack((u, 1-v, np.ones_like(u), alpha))
+
+    # cutting along the scroll center (mask left & right)
+    edge_x = np.interp(position_map[:, :, 2], scroll_center[:, 2], scroll_center[:, 0])
     mask_left = position_map[:, :, 0] - edge_x > 0
     mask_right = position_map[:, :, 0] - edge_x <= 0
 
-    position_map_left = np.copy(position_map)
-    position_map_right = np.copy(position_map)
-    position_map_left[mask_left, -1] = 0
-    position_map_right[mask_right, -1] = 0
-
-    # use opacity value to distinguish between different areas
-    binary_img_left = (position_map_left[:, :, 3] > 0.95).astype(np.uint8)
-    binary_img_right = (position_map_right[:, :, 3] > 0.95).astype(np.uint8)
-    contours_left, _ = cv2.findContours(binary_img_left, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    contours_right, _ = cv2.findContours(binary_img_right, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-    # draw & save each contour mask (left, right)
-    total_area = position_map.shape[0] * position_map.shape[1]
-
-    mask_count = 0
-    for i, contour in enumerate(contours_left):
-        area = cv2.contourArea(contour)
-        if (area / total_area < 0.0005): continue
-
-        mask_count += 1
-        mask = np.zeros_like(binary_img_left)
-
-        mask_name = f'{segmentID}_l{mask_count}_mask.png'
-        uv_name = f'{segmentID}_l{mask_count}_uv.png'
-        d_name = f'{segmentID}_l{mask_count}_d.png'
-
-        cv2.drawContours(mask, [contour], -1, 255, thickness=cv2.FILLED)
-        cv2.imwrite(mask_name, mask)
-        gen_sub_uv(mask_name, uv_name, position_map)
-        gen_sub_d(uv_name, d_name, position_map)
-        # if (mask_count == 1): gen_sub_d(uv_name, d_name, position_map)
-
-    mask_count = 0
-    for i, contour in enumerate(contours_right):
-        area = cv2.contourArea(contour)
-        if (area / total_area < 0.0005): continue
-
-        mask_count += 1
-        mask = np.zeros_like(binary_img_right)
-
-        mask_name = f'{segmentID}_r{mask_count}_mask.png'
-        uv_name = f'{segmentID}_r{mask_count}_uv.png'
-        d_name = f'{segmentID}_r{mask_count}_d.png'
-
-        cv2.drawContours(mask, [contour], -1, 255, thickness=cv2.FILLED)
-        cv2.imwrite(mask_name, mask)
-        gen_sub_uv(mask_name, uv_name, position_map)
-        gen_sub_d(uv_name, d_name, position_map)
-
-# Generate a new coordinate for each sub mask (with UV info)
-def gen_sub_uv(mask_name, uv_name, position_map):
-    # load sub mask
-    mask = cv2.imread(mask_name, cv2.IMREAD_UNCHANGED)
-
-    # uv coordinate with mask
-    h, w = mask.shape[:2]
-    alpha = mask / 255
-    u, v = np.meshgrid(np.linspace(0, 1, w), np.linspace(0, 1, h))
-    uv = np.dstack((u, 1-v, np.ones_like(u), alpha))
-
-    # find the contour & box region of that sub mask
-    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # find min rectangle box
+    gray = (position_map[:, :, 3] * 255).astype(np.uint8)
+    contours, _ = cv2.findContours(gray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     max_contour = max(contours, key=cv2.contourArea)
     rect = cv2.minAreaRect(max_contour)
     box = cv2.boxPoints(rect)
     box = np.intp(box)
-    # cv2.drawContours(mask, [box], 0, (0, 0, 255), 2)
 
     # box index sorting (make larger scroll z position is always at the top of the image)
     center, size, angle = rect
@@ -173,81 +119,143 @@ def gen_sub_uv(mask_name, uv_name, position_map):
     if (dt < 0): sorted_ind[3], sorted_ind[2] = sorted_ind[2], sorted_ind[3]
     sorted_box = box[sorted_ind]
 
-    # crop that region with sub mask & uv info
-    w, h = int(size[0]), int(size[1])
+    # generate cropped uv map
+    center, size, angle = rect
+    wb, hb = int(size[0]), int(size[1])
     src_pts = sorted_box.astype('float32')
-    dst_pts = np.array([[0, h-1], [w-1, h-1], [w-1, 0], [0, 0]], dtype='float32')
+    dst_pts = np.array([[0, hb-1], [wb-1, hb-1], [wb-1, 0], [0, 0]], dtype='float32')
     matrix = cv2.getPerspectiveTransform(src_pts, dst_pts)
 
-    cropped_image = cv2.warpPerspective(uv, matrix, (w, h))
-    cropped_image = (cropped_image * 65535).astype(np.uint16)
-    cropped_image = cv2.cvtColor(cropped_image, cv2.COLOR_RGBA2BGRA)
-    cv2.imwrite(uv_name, cropped_image)
+    uv_map_left = np.copy(uv_map)
+    uv_map_right = np.copy(uv_map)
+    uv_map_left[mask_left, -1] = 0
+    uv_map_right[mask_right, -1] = 0
+    uv_map_left = cv2.warpPerspective(uv_map_left, matrix, (wb, hb))
+    uv_map_right = cv2.warpPerspective(uv_map_right, matrix, (wb, hb))
 
+    # find its contours
+    binary_img_left = np.where(uv_map_left[:, :, 3] > 0.95, 255, 0).astype(np.uint8)
+    binary_img_right = np.where(uv_map_right[:, :, 3] > 0.95, 255, 0).astype(np.uint8)
+    contours_left, _ = cv2.findContours(binary_img_left, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours_right, _ = cv2.findContours(binary_img_right, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # save each contour uv map (left, right)
+    total_area = position_map.shape[0] * position_map.shape[1]
+
+    uv_map_left = (uv_map_left * 65535).astype(np.uint16)
+    uv_map_right = (uv_map_right * 65535).astype(np.uint16)
+    uv_map_left = cv2.cvtColor(uv_map_left, cv2.COLOR_RGBA2BGRA)
+    uv_map_right = cv2.cvtColor(uv_map_right, cv2.COLOR_RGBA2BGRA)
+
+    mask_count = 0
+    for i, contour in enumerate(contours_left):
+        area = cv2.contourArea(contour)
+        if (area / total_area < 0.0005): continue
+
+        mask_count += 1
+        uv_name = f'{segmentID}_l{mask_count}_uv.png'
+        d_name = f'{segmentID}_l{mask_count}_d.png'
+
+        mask = np.zeros_like(binary_img_left).astype(np.uint16)
+        cv2.drawContours(mask, [contour], -1, 65535, thickness=cv2.FILLED)
+        uv_map_left[:, :, 3] = mask
+        [x, y, w, h] = cv2.boundingRect(contour)
+        cv2.imwrite(uv_name, uv_map_left[:, x:(x+w), :])
+
+        gen_sub_d(uv_name, d_name, position_map)
+        # if (mask_count == 2): gen_sub_d(uv_name, d_name, position_map)
+
+    mask_count = 0
+    for i, contour in enumerate(contours_right):
+        area = cv2.contourArea(contour)
+        if (area / total_area < 0.0005): continue
+
+        mask_count += 1
+        uv_name = f'{segmentID}_r{mask_count}_uv.png'
+        d_name = f'{segmentID}_r{mask_count}_d.png'
+
+        mask = np.zeros_like(binary_img_right).astype(np.uint16)
+        cv2.drawContours(mask, [contour], -1, 65535, thickness=cv2.FILLED)
+        uv_map_right[:, :, 3] = mask
+        [x, y, w, h] = cv2.boundingRect(contour)
+        cv2.imwrite(uv_name, uv_map_right[:, x:(x+w), :])
+
+        gen_sub_d(uv_name, d_name, position_map)
+
+# Generate distance map for each sub segment
 def gen_sub_d(uv_name, d_name, position_map):
-    src  = cv2.imread(uv_name, cv2.IMREAD_UNCHANGED)
-    src  = cv2.cvtColor(src, cv2.COLOR_BGRA2RGBA)
-    gray = (src[:, :, 3] * 255).astype(np.uint8)
+    # load uv map & find contour
+    uv_map = cv2.imread(uv_name, cv2.IMREAD_UNCHANGED)
+    uv_map = cv2.cvtColor(uv_map, cv2.COLOR_BGRA2RGBA)
+    uv_map = uv_map.astype(np.float32) / 65535
+    h, w   = uv_map.shape[:2]
 
+    gray = (uv_map[:, :, 3] * 255).astype(np.uint8)
     contours, _ = cv2.findContours(gray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    max_contour = max(contours, key=cv2.contourArea)
-    rect = cv2.minAreaRect(max_contour)
-    box = cv2.boxPoints(rect)
-    box = np.intp(box)
-    # cv2.drawContours(src, [box], 0, (0, 0, 65535), 2)
+    contour = max(contours, key=cv2.contourArea)[:, 0, :]
 
-    m = max_contour[:, 0, :]
-    uvv = src[m[:, 1], m[:, 0]][:, :2] / 65535
-    h, w = position_map.shape[:2]
-    p  = position_map[((1-uvv[:, 1]) * (h-1)).astype(int), (uvv[:, 0] * (w-1)).astype(int)][:, :3].astype(float)
-    h, w = src.shape[:2]
+    # find 3d point positions on that contour
+    hp, wp = position_map.shape[:2]
+    uv = uv_map[contour[:, 1], contour[:, 0], :2]
+    uv[:, 0] = (wp - 1) * uv[:, 0]
+    uv[:, 1] = (hp - 1) * (1 - uv[:, 1])
+    uv = uv.astype(int)
+    p = position_map[uv[:, 1], uv[:, 0], :3]
 
+    # use histogram to find top & bottom edge (with the same z value)
     gap = 0.01
     bins = np.arange(0, 1+gap, gap)
-    weights = np.power(m[:, 1] / h - 0.5, 2) * 4
+    weights = np.power(contour[:, 1] / h - 0.5, 2) * 4
     hist, edges = np.histogram(p[:, 2], bins=bins, weights=weights)
-    peak_regions = np.argsort(hist)[-2:]
+    peaks = np.argsort(hist)[-2:]
+    if(abs(peaks[-1] - peaks[-2]) == 1): peaks[-2] = np.argsort(hist)[-3]
 
-    if(abs(peak_regions[-1] - peak_regions[-2]) == 1):
-        peak_regions[-2] = np.argsort(hist)[-3]
-
+    # save 4 corner indices
     corner_list = []
+    for peak in peaks:
+        z_lower = edges[peak]
+        z_upper = edges[peak + 1]
+        peak_ind = np.where((p[:, 2] > z_lower) & (p[:, 2] < z_upper))[0]
 
-    for region in peak_regions:
-        indices_in_region = np.where((p[:, 2] >= edges[region]) & (p[:, 2] <= edges[region + 1]))[0]
-
-        if indices_in_region.size > 0:
+        if peak_ind.size > 0:
             inside = False
             confident = 0
             threshold = 10
-            avg_value = np.mean(p[indices_in_region, 2])
+            avg_value = np.mean(p[peak_ind, 2])
 
             for i in range(len(p[:, 2])):
-                current_state = np.abs(p[i, 2] - avg_value) <= 0.03
+                current_state = np.abs(p[i, 2] - avg_value) <= 0.005
 
                 if(current_state != inside): confident += 1
+                if(current_state == inside): confident = max(confident - 1, 0)
                 if(confident == threshold):
-                    inside = not inside
                     confident = 0
+                    inside = not inside
                     index = i - threshold + 1
-                    if (index < 10): continue
+                    if (index < 5): continue
                     corner_list.append(index)
 
-    x_ind = sorted(corner_list, key=lambda i: m[i, 0])
+    if(len(corner_list) < 4): corner_list.append(0)
+    y_ind = sorted(corner_list, key=lambda i: contour[i, 1])
 
-    tl = min(x_ind[:2], key=lambda i: m[i, 1])
-    bl = max(x_ind[:2], key=lambda i: m[i, 1])
-    tr = min(x_ind[2:], key=lambda i: m[i, 1])
-    br = max(x_ind[2:], key=lambda i: m[i, 1])
+    # tl = min(y_ind[:2], key=lambda i: contour[i, 0])
+    # tr = max(y_ind[:2], key=lambda i: contour[i, 0])
+    # bl = min(y_ind[2:], key=lambda i: contour[i, 0])
+    # br = max(y_ind[2:], key=lambda i: contour[i, 0])
 
-    h, w = src.shape[:2]
-    m = m.astype('float')
-    m /= np.array([w, h])
-    mt = np.concatenate((m[tl:0:-1], m[-1:tr:-1]), axis=0)
+    tl = sorted(corner_list)[0]
+    bl = sorted(corner_list)[1]
+    br = sorted(corner_list)[2]
+    tr = sorted(corner_list)[3]
 
-    dl = np.interp(np.linspace(0, 1, h), m[tl:bl, 1], m[tl:bl, 0])
-    dr = np.interp(np.linspace(0, 1, h), m[tr:br:-1, 1], m[tr:br:-1, 0])
-    db = np.interp(np.linspace(0, 1, w), m[bl:br, 0], m[bl:br, 1])
+    h, w = uv_map.shape[:2]
+    contour = contour.astype('float')
+    contour /= np.array([w, h])
+    mt = np.concatenate((contour[tl:0:-1], contour[-1:tr:-1]), axis=0)
+
+    dl = np.interp(np.linspace(0, 1, h), contour[tl:bl, 1], contour[tl:bl, 0])
+    dr = np.interp(np.linspace(0, 1, h), contour[tr:br:-1, 1], contour[tr:br:-1, 0])
+    db = np.interp(np.linspace(0, 1, w), contour[bl:br, 0], contour[bl:br, 1])
     dt = np.interp(np.linspace(0, 1, w), mt[:, 0], mt[:, 1])
 
     dr = 1 - dr
@@ -258,14 +266,22 @@ def gen_sub_d(uv_name, d_name, position_map):
     img[:, :, 1] = (dr[:, np.newaxis] * 65535).astype(np.uint16)
     img[:, :, 2] = (dt[np.newaxis, :] * 65535).astype(np.uint16)
     img[:, :, 3] = (db[np.newaxis, :] * 65535).astype(np.uint16)
-    img = img[:, :, [2, 1, 0, 3]]
+    img = 65535 - img
+    img = cv2.cvtColor(img, cv2.COLOR_RGBA2BGRA)
     cv2.imwrite(d_name, img)
 
-    # cv2.drawContours(src, [max_contour[:, 0, :][tl:bl]], 0, (0, 0, 65535), 2)
-    # cv2.drawContours(src, [max_contour[:, 0, :][br:tr]], 0, (0, 65535, 0), 2)
-    # cv2.imshow('image', src)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
+    l_line = np.column_stack((w * dl, h * np.linspace(0, 1, h))).astype(np.int32)
+    r_line = np.column_stack((w * (1 - dr), h * np.linspace(0, 1, h))).astype(np.int32)
+    t_line = np.column_stack((w * np.linspace(0, 1, w), h * dt)).astype(np.int32)
+    b_line = np.column_stack((w * np.linspace(0, 1, w), h * (1- db))).astype(np.int32)
+
+    cv2.polylines(img, [l_line], isClosed=False, color=(0, 0, 65535), thickness=2)
+    cv2.polylines(img, [r_line], isClosed=False, color=(0, 65535, 0), thickness=2)
+    cv2.polylines(img, [t_line], isClosed=False, color=(65535, 0, 0), thickness=2)
+    cv2.polylines(img, [b_line], isClosed=False, color=(0, 0, 0), thickness=2)
+    cv2.imshow('image', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 # w, h = 17381, 13513
 w, h = 1738, 1351
@@ -274,15 +290,9 @@ x_max, y_max, z_max = 8096, 7888, 14370
 # position map generate
 # gen_pos_map(w, h, x_max, y_max, z_max)
 
-# sub mask & sub distance map generate
-clustering(x_max, y_max, z_max)
+# sub uv & distance map generate
+gen_sub_uv(x_max, y_max, z_max)
 
-
-# filename = '20230702185753_r3_d.png'
-# img = cv2.imread(filename, cv2.IMREAD_UNCHANGED)
-# img = (img / 65535 * 255).astype(np.uint8)
-# img = img[:, :, [0, 2, 1, 3]]
-# cv2.imwrite('ok.png', img)
 
 
 
