0a943e5889b4ffdae4c71f60f001e9240d5b4a03
Hsiao Yao <tomtogo1260@gmail.com>
2023-12-30 18:15:09 +0800

    add meta json

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 import cv2
+import json
 import numpy as np
 from scipy.interpolate import griddata, splprep, splev
 
@@ -65,8 +66,8 @@ def gen_pos_normal(segmentID, x_max, y_max, z_max):
 
     # generate the position & normal map (w, h, rgba)
     position_map = griddata(uv, p, (grid_u, grid_v), method='nearest', fill_value=0)
-    position_map = np.clip(position_map, 0, 1)
     normal_map   = griddata(uv, n, (grid_u, grid_v), method='nearest', fill_value=0)
+    position_map = np.clip(position_map, 0, 1)
     normal_map   = np.clip(normal_map, 0, 1)
 
     # remove the part outside the mask (opacity 0)
@@ -85,10 +86,9 @@ def gen_pos_normal(segmentID, x_max, y_max, z_max):
     cv2.imwrite(os.path.join(segmentID, normal_map_name), normal_map)
 
 # Generate uv map for each sub segment
-def gen_sub_uv(x_max, y_max, z_max):
+def gen_sub_uv(segmentID, x_max, y_max, z_max):
     center_name  = 'scroll1_center.obj'
-    pos_map_name = '20230702185753.png'
-    segmentID    = '20230702185753'
+    pos_map_name = f'{segmentID}_positions.png'
 
     # load scroll center into (used for cutting)
     data    = parse_obj(center_name)
@@ -96,7 +96,7 @@ def gen_sub_uv(x_max, y_max, z_max):
     scroll_center /= np.array([x_max, y_max, z_max])
 
     # load position map (rgba 0~1)
-    position_map = cv2.imread(pos_map_name, cv2.IMREAD_UNCHANGED)
+    position_map = cv2.imread(os.path.join(segmentID, pos_map_name), cv2.IMREAD_UNCHANGED)
     position_map = cv2.cvtColor(position_map, cv2.COLOR_BGRA2RGBA)
     position_map = position_map.astype(np.float32) / 65535
 
@@ -161,40 +161,43 @@ def gen_sub_uv(x_max, y_max, z_max):
     uv_map_left = cv2.cvtColor(uv_map_left, cv2.COLOR_RGBA2BGRA)
     uv_map_right = cv2.cvtColor(uv_map_right, cv2.COLOR_RGBA2BGRA)
 
+    contours_left = [(c, 'l') for c in contours_left]
+    contours_right = [(c, 'r') for c in contours_right]
+    contours = contours_left + contours_right
+    contours = sorted(contours, key=lambda c: cv2.boundingRect(c[0])[0], reverse=True)
+
     mask_count = 0
-    for i, contour in enumerate(contours_left):
+    chunks = []
+    for i, (contour, label) in enumerate(contours):
         area = cv2.contourArea(contour)
         if (area / total_area < 0.0005): continue
 
         mask_count += 1
-        uv_name = f'{segmentID}_l{mask_count}_uv.png'
-        d_name = f'{segmentID}_l{mask_count}_d.png'
+        uv_name = os.path.join(segmentID, f'{segmentID}_{label}{mask_count}_uv.png')
+        d_name = os.path.join(segmentID, f'{segmentID}_{label}{mask_count}_d.png')
 
-        mask = np.zeros_like(binary_img_left).astype(np.uint16)
+        uv_map = uv_map_left if (label == 'l') else uv_map_right
+        mask = np.zeros_like(uv_map[:, :, 3]).astype(np.uint16)
         cv2.drawContours(mask, [contour], -1, 65535, thickness=cv2.FILLED)
-        uv_map_left[:, :, 3] = mask
+        uv_map[:, :, 3] = mask
         [x, y, w, h] = cv2.boundingRect(contour)
-        cv2.imwrite(uv_name, uv_map_left[:, x:(x+w), :])
+        cv2.imwrite(uv_name, uv_map[:, x:(x+w), :])
 
-        gen_sub_d(uv_name, d_name, position_map)
-        # if (mask_count == 2): gen_sub_d(uv_name, d_name, position_map)
+        w, h, l, r = gen_sub_d(uv_name, d_name, position_map)
+        # if (mask_count == 1): gen_sub_d(uv_name, d_name, position_map)
 
-    mask_count = 0
-    for i, contour in enumerate(contours_right):
-        area = cv2.contourArea(contour)
-        if (area / total_area < 0.0005): continue
+        # save meta info
+        chunk = {}
+        chunk['id'] = mask_count
+        chunk['uv'] = f'{segmentID}_{label}{mask_count}_uv.png'
+        chunk['d'] = f'{segmentID}_{label}{mask_count}_d.png'
+        chunk['width'] = int(w)
+        chunk['height'] = int(h)
+        chunk['l'] = int(l)
+        chunk['r'] = int(r)
+        chunks.append(chunk)
 
-        mask_count += 1
-        uv_name = f'{segmentID}_r{mask_count}_uv.png'
-        d_name = f'{segmentID}_r{mask_count}_d.png'
-
-        mask = np.zeros_like(binary_img_right).astype(np.uint16)
-        cv2.drawContours(mask, [contour], -1, 65535, thickness=cv2.FILLED)
-        uv_map_right[:, :, 3] = mask
-        [x, y, w, h] = cv2.boundingRect(contour)
-        cv2.imwrite(uv_name, uv_map_right[:, x:(x+w), :])
-
-        gen_sub_d(uv_name, d_name, position_map)
+    return chunks
 
 # Generate distance map for each sub segment
 def gen_sub_d(uv_name, d_name, position_map):
@@ -289,10 +292,9 @@ def gen_sub_d(uv_name, d_name, position_map):
     t_line = np.column_stack((w * np.linspace(0, 1, w), h * dt)).astype(np.int32)
     b_line = np.column_stack((w * np.linspace(0, 1, w), h * (1- db))).astype(np.int32)
 
-    print('width', w)
-    print('height', h)
-    print('left', np.mean(l_line, axis=0)[0].astype(int))
-    print('right', w- np.mean(r_line, axis=0)[0].astype(int))
+    l = np.mean(l_line, axis=0)[0].astype(int)
+    r = np.mean(r_line, axis=0)[0].astype(int)
+    r = w - r
 
     cv2.polylines(img, [l_line], isClosed=False, color=(0, 0, 65535), thickness=2)
     cv2.polylines(img, [r_line], isClosed=False, color=(0, 65535, 0), thickness=2)
@@ -302,13 +304,32 @@ def gen_sub_d(uv_name, d_name, position_map):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
+    return w, h, l, r
+
+def save_meta(segmentID, chunks):
+    info = {}
+    info['id'] = segmentID
+    info['labels'] = ''
+    info['positions'] = f'{segmentID}_positions.png'
+    info['normals'] = f'{segmentID}_normals.png'
+    info['chunks'] = chunks
+
+    meta = {}
+    meta['segment'] = [ info ]
+    with open('meta.json', "w") as outfile: json.dump(meta, outfile, indent=4)
+
 x_max, y_max, z_max = 8096, 7888, 14370
 
-segmentID = '20230702185753'
+# segmentID = '20230702185753'
+segmentID = '20231031143852'
 if not os.path.exists(segmentID): os.makedirs(segmentID)
 
 # position & normal map generate
-gen_pos_normal(segmentID, x_max, y_max, z_max)
+# gen_pos_normal(segmentID, x_max, y_max, z_max)
 
 # sub uv & distance map generate
-# gen_sub_uv(x_max, y_max, z_max)
+chunks = gen_sub_uv(segmentID, x_max, y_max, z_max)
+
+# save meta info as json
+save_meta(segmentID, chunks)
+
